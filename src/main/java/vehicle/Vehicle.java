63222db5f18424afb6e89b9bd43f00dbe9f96c66
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-05-16 15:39:16 +0100

    Feat: Vehicle methods for rotating left and right added including test cases

--- src/main/java/vehicle/Vehicle.java
+++ src/main/java/vehicle/Vehicle.java
@@ -1,9 +1,10 @@
 package vehicle;
 
-import environment.Plateau;
+import input.layer.CompassDirection;
 import input.layer.Position;
 
 public class Vehicle {
+
     public Vehicle(Position position) {
         this.position = position;
     }
@@ -23,8 +24,30 @@ public class Vehicle {
         }
     }
 
-    public void rotate(){
+    public void rotateLeft(){
+        switch (position.getFacing()){
+            case N :position.setFacing(CompassDirection.W);
+                break;
+            case E: position.setFacing(CompassDirection.N);
+                break;
+            case S: position.setFacing(CompassDirection.E);
+                break;
+            case W: position.setFacing(CompassDirection.S);
+                break;
+        }
+    }
 
+    public void rotateRight(){
+        switch (position.getFacing()){
+            case N :position.setFacing(CompassDirection.E);
+                break;
+            case E: position.setFacing(CompassDirection.S);
+                break;
+            case S: position.setFacing(CompassDirection.W);
+                break;
+            case W: position.setFacing(CompassDirection.N);
+                break;
+        }
     }
 
 }
--- src/test/java/vehicle/VehicleTest.java
+++ src/test/java/vehicle/VehicleTest.java
@@ -26,6 +26,30 @@ class VehicleTest {
     }
 
     @Test
-    void rotate() {
+    void rotateLeft() {
+        Position position = new Position(0, 0, CompassDirection.N);
+        Vehicle vehicle = new Vehicle(position);
+
+        position.setFacing(CompassDirection.N);
+        vehicle.rotateLeft();
+        assertEquals(CompassDirection.W, position.getFacing());
+        position.setFacing(CompassDirection.E);
+        vehicle.rotateLeft();
+        assertEquals(CompassDirection.N, position.getFacing());
     }
+
+    @Test
+    void rotateRight() {
+        Position position = new Position(0, 0, CompassDirection.N);
+        Vehicle vehicle = new Vehicle(position);
+
+        position.setFacing(CompassDirection.N);
+        vehicle.rotateRight();
+        assertEquals(CompassDirection.E, position.getFacing());
+        position.setFacing(CompassDirection.E);
+        vehicle.rotateRight();
+        assertEquals(CompassDirection.S, position.getFacing());
+    }
+
+
 }
