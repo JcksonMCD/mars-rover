2470b07f17c44895f3458b48ef5a110f7215ba60
Jackson MacDonald <jacksonmacdonald@gmail.com>
2024-05-16 12:07:02 +0100

    Feat: Adding data validation and further test cases for PlateauSizeParser

--- src/main/java/org/northcoders/fundamentals/JavaPlatform/InputParser.java
+++ src/main/java/org/northcoders/fundamentals/JavaPlatform/InputParser.java
@@ -5,9 +5,11 @@ public class InputParser {
     // Takes input String and parses to PlateauSize
     public PlateauSize plateauSizeParser(String input){
         String[] separatedInput = input.split(" ");
-        return new PlateauSize(Integer.parseInt(separatedInput[0]), Integer.parseInt(separatedInput[1]));
+        if ((Integer.parseInt(separatedInput[0]) > 0) && (Integer.parseInt(separatedInput[1]) > 0)) {
+            return new PlateauSize(Integer.parseInt(separatedInput[0]), Integer.parseInt(separatedInput[1]));
+        } else {
+            throw new RuntimeException("Plateau size inputs need to be more than zero.");
+        }
     }
-
-
-
+    
 }
--- src/test/java/org/northcoders/fundamentals/JavaPlatform/InputParserTest.java
+++ src/test/java/org/northcoders/fundamentals/JavaPlatform/InputParserTest.java
@@ -12,5 +12,13 @@ class InputParserTest {
         PlateauSize plateauSize = new PlateauSize(5 , 5);
         assertEquals(5 , inputParser.plateauSizeParser("5 5").getMAX_X());
         assertEquals(5 , inputParser.plateauSizeParser("5 5").getMAX_Y());
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("0 0"));
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("0 1"));
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("1 0"));
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("-1 -1"));
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("1 -1"));
+        assertThrows(RuntimeException.class, () -> inputParser.plateauSizeParser("-1 1"));
     }
+
+
 }
